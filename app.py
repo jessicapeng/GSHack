0477a018dd613392524cfedf36ba489e5f4cde61
Jessica Peng <jessica123.peng@gmail.com>
2020-12-12 19:12:33 -0800

    Created parsing from frontend of dates

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ IPv6 Sec-Check Main Controller app.py
 #imports and dependencies for main controller - lib
 from flask import Flask, render_template, request, jsonify
 from datetime import datetime
-from api import script
+#from api import script
 
 import json
 import requests 
@@ -28,9 +28,14 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
     if request.method == "POST":
+        form = request.form
         print(request.form)
-        
+        start_date=form["s-year"]+"-"+form["s-month"]+"-"+form["s-year"]
+        end_date=form["e-year"]+"-"+form["e-month"]+"-"+form["e-year"]
+        print("start_date", start_date, "end_date", end_date)
+        #call script.runplots, etc; 
         return render_template("plot.html")
+    
 
     
 #start the server
